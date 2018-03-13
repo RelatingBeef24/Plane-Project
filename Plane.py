374cfd4a5368f80318aa1af7c2fa1665951c29b1
RelatingBeef24 <36928526+RelatingBeef24@users.noreply.github.com>
2018-03-13 11:02:57 -0500

    Add files via upload

--- Plane.py
+++ Plane.py
@@ -9,6 +9,9 @@ class Fuselage(object):
         base = Polygon(Point(500,250), Point(550,260),Point(580,270),Point(590,275),Point(600,290),Point(600,390),Point(600,400), Point(575,430), Point(550,450),Point(450,450),Point(425,430),Point(400,400),Point(400,390),Point(400,290),Point(415,275),Point(450,260))
         base.setFill(self.color)
         base.draw(win)
+        arc = Arc(Point(400, 375), Point(600, 450), 15, 150)
+        arc.draw(win)
+        arc.setFill("grey")
 
 class Wings(object):
     def __init__(self, color):
@@ -27,23 +30,51 @@ class Wings(object):
         wingP2.draw(win)
         wingP2.setOutline(self.color)
         wingP2.setFill(self.color)
-        wing2p1 = Rectangle(Point(950, 325), Point(600, 350))
-        wingTip = Arc(Point(975, 325), Point(950, 350), 90, 170)
-        wingTip.draw(win)
+        wing2p1 = Rectangle(Point(964, 325), Point(600, 350))
+        wingTip2 = Arc(Point(975, 325), Point(950, 350), 90, -170)
+        wing2P2 = Polygon(Point(960, 350), Point(600, 400), Point(600, 330), Point(960, 330))
+        wingTip2.draw(win)
         wing2p1.draw(win)
+        wing2P2.draw(win)
+        wingTip2.setFill(self.color)
+        wingTip2.setOutline(self.color)
+        wing2p1.setFill(self.color)
+        wing2p1.setOutline(self.color)
+        wing2P2.setFill(self.color)
+        wing2P2.setOutline(self.color)
 
-# class Engine(object):
-#     def __init__(self, size):
-#         self.size = size
-#
-#     def buildEngine(self):
-#         base = RoundedRectangle(Point(34))
+class Engine(object):
+    def __init__(self, size = 0):
+        self.size = size
+
+    def buildBase(self,win):
+        base = RoundedRectangle(Point(225 - (self.size*10),312 - (self.size*10)),Point(325 + (self.size*10),400 + (self.size*10)))
+        base.draw(win)
+        base2 = RoundedRectangle(Point(675 - (self.size*10),312 - (self.size*10)), Point(775 + (self.size*10), 400 + (self.size*10)))
+        base2.draw(win)
+
+    def buildHub(self,win):
+        cap = Circle(Point(275,356), 5 + self.size)
+        cap.draw(win)
+        cap2 = Circle(Point(725,356), 5 + self.size)
+        cap2.draw(win)
+
+class Prop(Engine):
+    def __init__(self, props):
+        super(Prop, self).__init__(props)
+        self.props = props
+    def prop(self, props):
+        blade = Rectangle
 class Plane(object):
     def __init__(self,win):
         fuselage = Fuselage("green")
         fuselage.buildFuselage(win)
         wing = Wings("red")
         wing.buildWings(win)
+        engine = Engine(0)
+        engine.buildBase(win)
+        engine.buildHub(win)
+
 def main():
     window = GraphWin("Plane", 1000, 650)
     tc = Plane(window)
