eacdd5b4f07b5946f5fa82447cf0fcd3db62d056
RelatingBeef24 <36928526+RelatingBeef24@users.noreply.github.com>
2018-03-13 19:46:40 -0500

    Delete Fuselage.py

--- Fuselage.py
+++
@@ -1,9 +0,0 @@
-from Graphics import *
-class Fuselage(object):
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
-        self.color = color
-
-    def buildFuselage(self, x, y, color):
-        
