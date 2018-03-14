dd9af2807ab34448d4837461227cd31de206ac19
RelatingBeef24 <36928526+RelatingBeef24@users.noreply.github.com>
2018-03-14 10:33:34 -0500

    Updated Plane
    Added some different colors

--- Plane.py
+++ Plane.py
@@ -12,7 +12,7 @@ class Fuselage(object):
         base.draw(win)
         arc = Arc(Point(400, 375), Point(600, 450), 15, 150)
         arc.draw(win)
-        arc.setFill("grey")
+        arc.setFill(color_rgb(172,175,175))
 
 class Wings(object):
     def __init__(self,win):
@@ -50,6 +50,7 @@ class Engine(object):
     def __init__(self, size,win):
         self.size = size
         self.buildBase(win)
+        self.buildCowling(win)
         self.buildHub(win)
 
     def buildBase(self,win):
@@ -57,8 +58,16 @@ class Engine(object):
         base.draw(win)
         base2 = RoundedRectangle(Point(675 - (self.size*5),312 - (self.size*5)), Point(775 + (self.size*5), 400 + (self.size*5)))
         base2.draw(win)
-        base.setFill("green")
-        base2.setFill("green")
+        base.setFill(color_rgb(49,73,112))
+        base2.setFill(color_rgb(49,73,112))
+
+    def buildCowling(self,win):
+        cap = Circle(Point(275,356), 25 + self.size)
+        cap.draw(win)
+        cap2 = Circle(Point(725,356), 25 + self.size)
+        cap2.draw(win)
+        cap.setFill("black")
+        cap2.setFill("black")
 
     def buildHub(self,win):
         cap = Circle(Point(275,356), 5 + self.size)
@@ -68,28 +77,29 @@ class Engine(object):
         cap.setFill("maroon")
         cap2.setFill("maroon")
 
+
 class Prop(Engine):
     def __init__(self,win,size = 0):
         super(Prop, self).__init__(size,win)
         self.buildProp(win)
     def buildProp(self,win):
 
-        blade = Rectangle(Point(270,351-self.size),Point(280,270 - self.size * 5))
+        blade = Rectangle(Point(270 - self.size,351-self.size),Point(280+self.size,270 - self.size * 5))
         blade.draw(win)
-        blade2 = Rectangle(Point(270,442+self.size* 5),Point(280,361 + self.size))
+        blade2 = Rectangle(Point(270 - self.size,442+self.size* 5),Point(280 + self.size,361 + self.size))
         blade2.draw(win)
         blade.setFill("grey")
         blade2.setFill("grey")
 
-        blade2ndEng = Rectangle(Point(720,351-self.size),Point(730,270 - self.size * 5))
+        blade2ndEng = Rectangle(Point(720 - self.size,351-self.size),Point(730+self.size,270 - self.size * 5))
         blade2ndEng.draw(win)
-        blade2ndEng2 = Rectangle(Point(720,442+self.size * 5),Point(730,361 + self.size))
+        blade2ndEng2 = Rectangle(Point(720- self.size,442+self.size * 5),Point(730 + self.size,361 + self.size))
         blade2ndEng2.draw(win)
         blade2ndEng.setFill("grey")
         blade2ndEng2.setFill("grey")
 class Plane(object):
     def __init__(self,win):
-        fuselage = Fuselage("blue",win)
+        fuselage = Fuselage(color_rgb(199,204,203),win)
         wing = Wings(win)
         # engine = Engine(0,win) #Don't worry about this for now
         prop = Prop(win,0)
@@ -98,6 +108,7 @@ class Plane(object):
 def main():
     window = GraphWin("Plane", 1000, 650)
     tc = Plane(window)
+    window.setBackground(color_rgb(123,224,221))
     window.getMouse()
     window.close()
 
